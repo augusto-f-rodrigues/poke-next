2fc6659340d71739a5019671bc16dcb14e6f9104
Augusto Rodrigues <augustofeliperodrigues1999@gmail.com>
2022-09-25 09:29:49 -0300

    fixing the responsibility of the navbar menu

--- src/components/templates/ListTemplate.tsx
+++ src/components/templates/ListTemplate.tsx
@@ -9,7 +9,7 @@ export default function LisTemplate(props: LisTemplateProps) {
   return (
     <li className="pr-2 pl-2">
       <Link href={props.link}>
-        <a className="text-lg text-white font-bold hover:text-purple-300 transition"> {props.title} </a>
+        <a className="text-xs sm:text-lg text-white font-bold hover:text-purple-300 transition"> {props.title} </a>
       </Link>
     </li>
   );
