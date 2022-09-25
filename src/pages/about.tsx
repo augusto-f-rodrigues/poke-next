0a0a642d0a4abf7d6382e544d4bfa591aa0f9628
Augusto Rodrigues <augustofeliperodrigues1999@gmail.com>
2022-09-25 17:14:02 -0300

    changing the readme file

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,18 +4,21 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>PokeNext - About</title>
-    </Head>
+      <Head>
+        <title>PokeNext - About</title>
+      </Head>
       <div className="flex flex-col w-full h-full items-center">
         <h1 className="text-purple-800 font-black text-2xl pb-3">
           About the project
         </h1>
         <p className="max-w-screen-md text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-          explicabo cumque iusto, voluptas aut assumenda repellat accusamus! In
-          ullam dolorum error est expedita ut vitae eveniet accusantium!
-          Laudantium, ab temporibus.
+          This project is a simple pokedex where you can access pokemon details
+          to see some basic information about it, hope you like it.
+        </p>{" "}
+        <p className="max-w-screen-md text-xl mt-3 mb-6">
+          A project that I had a lot of fun doing and I dedicated myself to
+          making it as responsive and aesthetic as possible with the knowledge I
+          have so far.
         </p>
         <Image
           src={"/images/gengar.png"}
